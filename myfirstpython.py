06c351855264d28a4f0a2fb2fd5a13ca78874154
Mark Revill <mark@mgrlabs.com>
2018-03-01 12:49:48 +1100

    python script test

--- myfirstpython.py
+++ myfirstpython.py
@@ -3,6 +3,9 @@ import boto3
 
 # Let's use Amazon S3
 s3 = boto3.resource('s3')
+instance = ec2.Instance('i-0c5b4a39d38e5a626')
 
-for bucket in s3.buckets.all():
-    print(bucket.name)
+#for bucket in s3.buckets.all():
+#    print(bucket.name)
+
+print(instance)
