6bb0afddf9cae13185046432426f8fc76a83701a
Mark Revill <mark@mgrlabs.com>
2018-03-01 13:44:41 +1100

    python script test

--- myfirstpython.py
+++ myfirstpython.py
@@ -1,26 +1,8 @@
 #!/usr/bin/python
 import boto3
+import csv
 
-
-# # Let's use Amazon S3
-# s3 = boto3.resource('s3')
-
-# ec2 = boto3.resource('ec2')
-# instance = ec2.Instance('i-0c5b4a39d38e5a626')
-
-# #for bucket in s3.buckets.all():
-# #    print(bucket.name)
-
-# for instance in ec2.instances.all():
-#     print(instance.instance_id, instance.state)
-
-
-# Create CloudWatch client
-cloudwatch = boto3.client('cloudwatch')
-
-# List metrics through the pagination interface
-paginator = cloudwatch.get_paginator('list_metrics')
-for response in paginator.paginate(Dimensions=[{'Name': 'LogGroupName'}],
-                                   MetricName='IncomingLogEvents',
-                                   Namespace='AWS/Logs'):
-    print(response['Metrics'])
+with open('ALH_CloudWatch.csv', 'rb') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print row
