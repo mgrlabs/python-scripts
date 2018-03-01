d1d6ce0124620308e1c2fbc769abd0dbf5893fa4
Mark Revill <mark@mgrlabs.com>
2018-03-01 14:03:24 +1100

    python script test

--- myfirstpython.py
+++ myfirstpython.py
@@ -2,7 +2,14 @@
 import boto3
 import csv
 
-with open('ALH_CloudWatch.csv') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        print row
+# with open('ALH_CloudWatch.csv') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print row
+
+with open('ALH_CloudWatch.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV:
+        # print(row)
+        print(row[0])
+        # print(row[0],row[1],row[2],)        
